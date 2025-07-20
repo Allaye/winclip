9b1e357807bcace0f3dd7b02bda84d539181cc86
Allaye <kolade199@gmail.com>
2025-07-20 18:34:04 +0100

    update absolute path reference

--- gui/styles/style.py
+++ gui/styles/style.py
@@ -1,8 +1,15 @@
+
+import gi
+import os
+gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk, Gdk
 
 def load_css():
     provider = Gtk.CssProvider()
-    provider.load_from_path("./styles/style.css")
+    # Get the directory where this file is located
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    css_path = os.path.join(current_dir, "style.css")
+    provider.load_from_path(css_path)
     Gtk.StyleContext.add_provider_for_display(
         Gdk.Display.get_default(),
         provider,
