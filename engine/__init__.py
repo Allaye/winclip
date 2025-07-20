62217ebff3c1e7d8509a7d50172c4b87f60437dc
Allaye <kolade199@gmail.com>
2025-07-20 18:34:33 +0100

    import module to context

---
+++ engine/__init__.py
@@ -0,0 +1,3 @@
+from .model import Clip
+from .storage import get_recent_clips, init_db, insert_clip
+from .monitor import ClipboardMonitor
---
+++ gui/__init__.py
@@ -0,0 +1,4 @@
+from .styles.style import load_css
+from .widgets.clip_card import ClipCard
+from .widgets.header_bar import HeaderBar
+from .winclip import ClipboardWindow
